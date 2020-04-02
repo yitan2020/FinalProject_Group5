0425c42b992ff5944c22872f2ea35dd259b1b1ed
yitan2020 <tan5@sheridancollege.ca>
2020-04-02 11:57:01 -0400

    create project

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+Yi Tan <tan5@sheridan.desire2learn.com>
+----------------------------------------------------------------------
+ */
+
+/* 
+ * File:   main.c
+ * Author: Yi Tan <tan5@sheridan.desire2learn.com>
+ *
+ * Created on April 2, 2020, 11:51 a.m.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
